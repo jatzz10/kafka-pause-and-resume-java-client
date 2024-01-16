b150e7ea2548ebd061e64f4e1a0dce089854632a
Jatin Sharma <jatinsharma2751997@gmail.com>
2024-01-16 14:02:14 +0530

    Initial directory and files

---
+++ src/main/java/org/kafka/pause/and/resume/java/client/Main.java
@@ -0,0 +1,11 @@
+package org.kafka.pause.and.resume.java.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
